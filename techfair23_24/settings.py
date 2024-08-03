12453c190016e7c1481a747b46f88eb989ba24d7
nareshrajesh <nareshrajesh787@gmail.com>
2024-08-03 18:20:49 -0400

    added allowed host

--- techfair23_24/settings.py
+++ techfair23_24/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qxycws@nw9-=(lv&3u(_$%+pi3jgx+se0k1h^ffbon$(3!9lj9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nareshrajesh.pythonanywhere.com']
 
 
 # Application definition
